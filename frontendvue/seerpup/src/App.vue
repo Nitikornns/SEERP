025147f6dbfd0c146937fdc556440a91d7d3d5f5
Ryeng8 <37680261+Ryeng8@users.noreply.github.com>
2020-05-15 17:41:45 +0700

    vue + tailwind

---
+++ frontendvue/seerpup/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <!-- <div id="nav">
+      <br>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+        <router-view/>
+          <Button class="bg-red-700 hover:bg-red-200 hover:text-blue-500 text-white font-bold py-2 px-4 rounded">ทดลอง Tailwind css</Button>
+
+  </div>
+</template>
+
+<style>
+</style>
