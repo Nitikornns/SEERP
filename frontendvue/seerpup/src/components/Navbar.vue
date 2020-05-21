4f613a603957b6afc39a9ad45a2d95451c0bc948
Ryeng8 <37680261+Ryeng8@users.noreply.github.com>
2020-05-21 12:08:12 +0700

    navbar&loginform-vue
    navbar&loginform-vue

---
+++ frontendvue/seerpup/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <nav class="flex items-center justify-between flex-wrap bg-teal-700 p-1">
+  <div class="flex items-center flex-shrink-0 text-white m-0">
+           <img class="object-contain h-16 w-12 ml-20 mr-20" src="selogo.png"/>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-red-800 hover:text-white hover:border-white">
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Docs
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+        Examples
+      </a>
+      <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
+        Blog
+      </a>
+    </div>
+    <div>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+    </div>
+  </div>
+</nav>
+
+</div>
+  
+</template>
+
+<script>
+export default {
+    name : "Navbar"
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontendvue/seerpup/src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <Navbar/>
+    <InputText/>
+</div>
+  
+</template>
+
+<script>
+import InputText from '@/components/InputText'
+import Navbar from '@/components/Navbar'
+
+export default {
+    name : "Login",
+    components : {
+        InputText,
+        Navbar,
+    },
+    data : () => ({
+
+    }),
+}
+</script>
+
+<style>
+
+</style>
