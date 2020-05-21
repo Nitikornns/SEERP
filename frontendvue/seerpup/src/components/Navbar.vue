f3351872261e0fa30454793ac95511c75152af17
Ryeng8 <37680261+Ryeng8@users.noreply.github.com>
2020-05-21 12:11:52 +0700

    commit

--- frontendvue/seerpup/src/components/Navbar.vue
+++ frontendvue/seerpup/src/components/Navbar.vue
@@ -11,17 +11,17 @@
   <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
     <div class="text-sm lg:flex-grow">
       <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Docs
+        หน้าหลัก
       </a>
       <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        Examples
+        ประวัติส่วนตัว
       </a>
       <a href="#responsive-header" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-        Blog
+        อาจารย์ที่ปรึกษา
       </a>
     </div>
     <div>
-      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+      <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Profile</a>
     </div>
   </div>
 </nav>
